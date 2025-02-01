144667ed86e6d95a511eb5e2ded29ce7cf0456eb
moaz <moaz43700@gmail.com>
2025-02-01 23:13:59 +0200

    que 2

--- Ass1ADVC#/Program.cs
+++ Ass1ADVC#/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Ass1ADVC_
 {
+
     #region 1. Create a generic Range<T> class that represents a range of values from a minimum value to a maximum value.
     //internal class Range<T> where T : IComparable<T>
     //{
@@ -30,51 +31,52 @@ namespace Ass1ADVC_
     #endregion
 
 
-    #region 2. You are given an ArrayList containing a sequence of elements
-    internal class ArrayList<T>
-    {
-        public static void ReverseArray<T>(List<T> list)
-        {
-            int left = 0;
-            int right = list.Count - 1;
-
-            while (left < right)
-            {
-                T temp = list[left];
-                list[left] = list[right];
-                list[right] = temp;
-
-                left++;
-                right--;
-            }
-        }
-
-        public static void PrintList<T>(List<T> list)
-        {
-            foreach (var item in list)
-            {
-                Console.Write(item + " ");
-            }
-            Console.WriteLine();
-        }
-    }
-    #endregion
-
     internal class Program
     {
+
+        #region 2. You are given an ArrayList containing a sequence of elements
+        //public static void ReverseArray<T>(List<T> list)
+        //{
+        //    int left = 0;
+        //    int right = list.Count - 1;
+
+        //    while (left < right)
+        //    {
+        //        T temp = list[left];
+        //        list[left] = list[right];
+        //        list[right] = temp;
+
+        //        left++;
+        //        right--;
+        //    }
+        //}
+        #endregion
+
+
+
         static void Main(string[] args)
         {
             //Range<int> intRange = new Range<int>(4, 6);
-
             //Console.WriteLine("Number is in range: " + intRange.IsInRange(6));
             //Console.WriteLine("Length of range: " + intRange.Length());
 
+            //Range<double> doubleRange = new Range<double>(1.5, 4.5);
+            //Console.WriteLine(doubleRange.IsInRange(3.2));
+            //Console.WriteLine(doubleRange.Length());
+
             //------------------------------------------------------------------//
 
-            List<int> intList = new List<int>() { 1 , 2 , 3};
-            Console.WriteLine("original integer list: ");
-    
-            PrintList(intList);
+            //List<int> myList = new List<int> { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine("Original List: " + string.Join(", ", myList.ToArray()));
+
+            //ReverseArray(myList);
+
+            //Console.WriteLine("Reversed List: " + string.Join(", ", myList.ToArray()));
+
+            //-------------------------------------------------------------------//
+
+
 
 
         }
